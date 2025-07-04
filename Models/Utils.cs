558d0fe9730729ce98fb6ba254c84a6b454190c2
疯の葉 <crazyleaf0912@outlook.com>
2025-07-04 23:37:44 +0800

    Little fixes.

--- Models/Utils.cs
+++ Models/Utils.cs
@@ -25,7 +25,6 @@ namespace InGameHUD.Models
         }
 
         public string SteamID { get; }
-        public string Language { get; set; } = "zh-Hans";
         public HUDPosition HUDPosition { get; set; } = HUDPosition.TopRight;
         public bool HUDEnabled { get; set; } = true;
         public Dictionary<string, string> CustomData { get; set; } = new();
